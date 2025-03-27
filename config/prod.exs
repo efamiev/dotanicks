ea6d8b95cac5207552dc5db1bcf527709a22848b
e.famiev <famiev91@gmail.com>
2025-03-27 13:26:58 +0500

    Change prod config

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dotanicks_web, DotanicksWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
